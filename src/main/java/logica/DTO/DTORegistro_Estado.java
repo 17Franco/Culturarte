6ca615664bd0d5f3dd99a710e181c32bfc014662
Nicolás N. <nic.n@outlook.es>
2025-08-14 13:56:18 -0300

    Se agrega Class Categoria, DTFecha, y varios DTO

---
+++ src/main/java/logica/DTO/DTORegistro_Estado.java
@@ -0,0 +1,72 @@
+package DTO;
+import DTFecha.DTFecha;
+import _enum.Estado;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DTORegistro_Estado 
+{
+    private DTFecha fechaReg;
+    private Estado estados;
+    private Set<DTORegistro_Estado> historial;
+
+    public DTORegistro_Estado() 
+    {
+        historial = new HashSet<>();
+    }
+
+    public DTORegistro_Estado(DTFecha _fechaReg, Estado _estados)
+    {
+        fechaReg = _fechaReg;
+        estados = _estados;
+        historial = new HashSet<>();
+    }
+
+    public DTORegistro_Estado(DTFecha _fechaReg, Estado _estados, Set<DTORegistro_Estado> _historial) 
+    {
+        fechaReg = _fechaReg;
+        estados = _estados;
+
+            if (_historial != null)
+            {
+                historial = _historial;
+            }
+
+            if (_historial == null)
+            {
+                historial = new HashSet<>();
+
+            }
+    }
+  
+    public DTFecha getFechaReg() 
+    {
+        return fechaReg;
+    }
+
+    public Estado getEstados() 
+    {
+        return estados;
+    }
+
+    public Set<DTORegistro_Estado> getHistorial() 
+    {
+        return historial;
+    }
+
+    public void setFechaReg(DTFecha _fechaReg) 
+    {
+        fechaReg = _fechaReg;
+    }
+
+    public void setEstados(Estado _estados) 
+    {
+        estados = _estados;
+    }
+
+    public void setHistorial(Set<DTORegistro_Estado> _historial) 
+    {
+        historial = _historial;
+    }
+}
