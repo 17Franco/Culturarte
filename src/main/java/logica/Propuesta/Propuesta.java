493b7c1cdae118e4002bcc841dd3c6187e87ab25
asus <asus@DESKTOP-49QNDDN.localdomain>
2025-08-15 01:18:10 -0300

    Clase: Propuesta y Enum: TipoRetorno

--- src/main/java/logica/Propuesta/Propuesta.java
+++ src/main/java/logica/Propuesta/Propuesta.java
@@ -4,10 +4,101 @@
  */
 package logica.Propuesta;
 
-/**
- *
- * @author fran
- */
+import logica._enum.TipoRetorno;
+import logica.DTO.DTFecha;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Propuesta {
     
+    String Titulo;
+    String Descripcion;
+    String Tipo;
+    String Lugar;
+    DTFecha Fecha;
+    String Precio;
+    String MontoTotal;
+    DTFecha FechaPublicacion;
+    String Retorno;
+ // private List<Registro_Estado> historialEstados = new ArrayList<>();
+            
+    public Propuesta(){}
+    public Propuesta(String Titulo,String Descripcion,String Tipo, String Lugar, DTFecha Fecha, String Precio, String MontoTotal,DTFecha FechaPublicacion,String Retorno)
+    {
+        this.Titulo=Titulo;
+        this.Descripcion=Descripcion;
+        this.Tipo=Tipo;
+        this.Lugar=Lugar;
+        this.Fecha=Fecha;
+        this.Precio=Precio;
+        this.MontoTotal=MontoTotal;
+        this.FechaPublicacion=FechaPublicacion;
+        this.Retorno=Retorno;
+    }
+    public  String getTitulo() {
+        return Titulo;
+    }
+    public  String getDescripcion() {
+        return Descripcion;
+    }
+    public  String getTipo() {
+        return Tipo;
+    }
+    public  String getLugar() {
+        return Lugar;
+    }
+    public  DTFecha getFecha() {
+        return Fecha;
+    }
+    public String getPrecio() {
+        return Precio;
+    }
+    public String getMontoTotal() {
+        return MontoTotal;
+    }
+    public DTFecha getFechaPublicacion() {
+        return FechaPublicacion;
+    }
+    public String getRetorno() {
+        return Retorno;
+    }
+  /*  public List<Registro_Estado> getHistorialEstados() {
+        return historialEstados;
+    }
+    */
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    public void setLugar(String lugar) {
+        Lugar = lugar;
+    }
+
+    public void setFecha(DTFecha fecha) {
+        Fecha = fecha;
+    }
+
+    public void setPrecio(String precio) {
+        Precio = precio;
+    }
+
+    public void setMontoTotal(String montoTotal) {
+        MontoTotal = montoTotal;
+    }
+
+    public void setFechaPublicacion(DTFecha fechaPublicacion) {
+        FechaPublicacion = fechaPublicacion;
+    }
+
+    public void setRetorno(String retorno) {
+        Retorno = retorno;
+    } 
 }
---
+++ src/main/java/logica/_enum/TipoRetorno.java
@@ -0,0 +1,7 @@
+package logica._enum;
+
+public enum TipoRetorno
+{   
+     Entrada_Gratis,
+     Porcentaje_de_Ganancia_por_concepto_de_ventas
+}
