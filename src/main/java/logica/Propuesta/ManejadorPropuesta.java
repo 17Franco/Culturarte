8dc0cd44dff2f3cf7dac97f6e1a4e8328834f3c2
klaas <klaas@EliteBook-8570P>
2025-08-19 01:45:04 -0300

    Se agregan funciones auxiliares (no hacer pull)

--- src/main/java/logica/Propuesta/ManejadorPropuesta.java
+++ src/main/java/logica/Propuesta/ManejadorPropuesta.java
@@ -7,6 +7,13 @@ package logica.Propuesta;
 import java.util.HashMap;
 import java.util.Map;
 import logica.Propuesta.Propuesta;
+import logica.DTO.DTOPropuesta;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Calendar;
+import logica.DTO.DTFecha;
+import java.util.Date;
 
 
 public class ManejadorPropuesta {
@@ -36,5 +43,47 @@ public class ManejadorPropuesta {
         } else {
             return false;
         }
+        
+        
+
     }
+        
+        public Set<DTOPropuesta> obtenerPropuestas(String Estado)
+        {
+            //La variable Estado permite elegir entre obtener un set por estado o todos los que haya. 
+            //Si pones "" te manda todas las propuestas.
+            
+            Set<DTOPropuesta> temp = new HashSet<>();
+            
+            Iterator <Map.Entry  <String, Propuesta> > ct;  //Se crea iterador
+            ct = propuestasp.entrySet().iterator();         //Se configura tipo de iterator
+
+            while (ct.hasNext()) 
+            {
+                Map.Entry <String, Propuesta> entry = ct.next();    //Pasa al siguiente
+                Propuesta punteroV = entry.getValue();
+
+                DTOPropuesta almacenTemp = new DTOPropuesta();
+                
+                if(Estado.isEmpty())    //Si no se especifica, se agregan todos.
+                {
+                    almacenTemp.extraerDatosPropuesta(punteroV);
+                    temp.add(almacenTemp);
+                }
+                else    //Si se especifica...
+                {
+                    if(punteroV.get)
+                    {
+                        almacenTemp.extraerDatosPropuesta(punteroV);
+                        temp.add(almacenTemp);
+                    }
+                }
+            }
+            
+            
+            
+            return temp;
+        }
+        
+        
 }
