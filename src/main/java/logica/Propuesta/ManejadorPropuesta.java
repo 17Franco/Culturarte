81c7337625fe12b0fa4f10fc732cfbfe87af44a3
klaas <klaas@EliteBook-8570P>
2025-08-19 17:33:23 -0300

    correcciones menores

--- src/main/java/logica/Propuesta/ManejadorPropuesta.java
+++ src/main/java/logica/Propuesta/ManejadorPropuesta.java
@@ -69,10 +69,9 @@ public class ManejadorPropuesta {
             } 
             else //Si se especifica...
             {
-                //if((punteroV.getUltimoEstado().getEstado()) == Estado.valueOf(estadoInput)) //Se compara el enum en la posicion actual con el string que ingresa.
-                {                System.out.print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
-                    //Pendiente solucionar problema con las funciones de la condición.
-                
+                if((punteroV.getUltimoEstado().getEstado()) == Estado.valueOf(estadoInput)) //Se compara el enum en la posicion actual con el string que ingresa.
+                {              
+                    //Pendiente de revisión hasta que se puedan ingresar un registro_estado a cada propuesta
                     almacenTemp.extraerDatosPropuesta(punteroV);
                     temp.add(almacenTemp);
                 }
