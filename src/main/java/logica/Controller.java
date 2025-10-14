d958aa6bc62404380026463748cc6e1291cc233b
Klaa5 <nic.n@outlook.es>
2025-10-14 20:49:54 -0300

    Agrego control extra para mejorar integridad en función de nuevo comentario

--- src/main/java/logica/Controller.java
+++ src/main/java/logica/Controller.java
@@ -484,7 +484,7 @@ public class Controller  implements IController {
     @Override
     public boolean nuevoComentario(String comentario,String userNick,String tituloPropuesta)
     {
-        if(userNick != null && mPropuesta.nuevoComentario(comentario, userNick, tituloPropuesta))
+        if(userNick != null && !userNick.isEmpty() && mPropuesta.nuevoComentario(comentario, userNick, tituloPropuesta))
         {
             return true;
         }
