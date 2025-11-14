68cf3f8d1295fb17e08be6aa22036340fc9c0f07
asus <asus@DESKTOP-49QNDDN.localdomain>
2025-11-14 19:51:17 -0300

    test

---
+++ src/main/java/logica/Propuesta/Comentario.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica.Propuesta;
+
+import jakarta.persistence.Embeddable;
+
+
+/**
+ *
+ * @author klaas
+ */
+
+@Embeddable
+public class Comentario 
+{
+
+    private String nickUsuario;
+    private String comentario;
+    
+    public Comentario() {}
+    
+    public Comentario(String nickUsuario, String comentario) 
+    {
+        this.nickUsuario = nickUsuario;
+        this.comentario = comentario;
+    }
+    
+    public String getNickUsuario() 
+    {
+        return nickUsuario;
+    }
+    
+    public void setNickUsuario(String nickUsuario) 
+    {
+        this.nickUsuario = nickUsuario;
+    }
+    
+    public String getComentario() 
+    {
+        return comentario;
+    }
+    
+    public void setComentario(String comentario) 
+    {
+        this.comentario = comentario;
+    }
+}
